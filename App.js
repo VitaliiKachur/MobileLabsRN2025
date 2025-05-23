5a8d45f1578d87919633e7840d55bc210d5e9571
Vttalii_Kachut <vtk241_kvv@student.ztu.edu.ua>
2025-05-24 00:15:49 +0300

    feat: integrate navigation and gesture handler in main App component

--- App.js
+++ App.js
@@ -1,31 +1,21 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
 import React from 'react';
-import { View, Text, StyleSheet, Dimensions } from 'react-native';
-import {
-  GestureHandlerRootView,
-  TapGestureHandler,
-  LongPressGestureHandler,
-  PanGestureHandler,
-  FlingGestureHandler,
-  PinchGestureHandler,
-  State,
-} from 'react-native-gesture-handler';
+import { GestureHandlerRootView } from 'react-native-gesture-handler';
+import { StyleSheet } from 'react-native';
+import { StatusBar } from 'expo-status-bar';
+
+import AppNavigator from './src/navigation/AppNavigator';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <GestureHandlerRootView style={styles.container}>
+      <StatusBar style="light" backgroundColor="#3b82f6" />
+      <AppNavigator />
+    </GestureHandlerRootView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
 });
